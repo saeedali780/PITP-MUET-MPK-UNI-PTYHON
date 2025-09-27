e26d072901693a23ee06fc1a14e28fef08b04959
Saeed Ali <saeedaliofficial77@gmail.com>
2025-09-27 18:24:21 +0500

    final commit

---
+++ Tasks & Projects/PITP HOMEWORK 26-SEP/calculateAreaAndPerimeter.py
@@ -0,0 +1,9 @@
+# Calculate Area and Perimeter of Rectangle
+length = float(input("Enter the Length of Reactangle: "))
+width = float(input("Enter the width of Reactangle: "))
+
+area = length * width
+perimeter = 2 *(length + width)
+
+print(f"Area: {area:.2f}")
+print(f"Perimeter: {perimeter:.2f}")
