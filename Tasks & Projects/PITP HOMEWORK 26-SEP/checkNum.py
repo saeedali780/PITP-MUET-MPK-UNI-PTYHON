662278c45152ceba09078db0708278e28acc61ae
Saeed Ali <saeedaliofficial77@gmail.com>
2025-09-27 18:03:59 +0500

    check num assigment completed

---
+++ Tasks & Projects/PITP HOMEWORK 26-SEP/checkNum.py
@@ -0,0 +1,8 @@
+# Check if Number is Positive, Negative, or Zero
+num = int(input("Enter a number: "))
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else: 
+    print("The number is zero")
